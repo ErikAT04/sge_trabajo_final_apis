dc818da0b2e5a67c39df106c43ea3ea9b45372fd
Erik Amo <aterik.dev@gmail.com>
2025-02-14 10:46:16 +0100

    Prueba bd online

--- app/database/database.py
+++ app/database/database.py
@@ -1,13 +1,22 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-user ="root"
-password = "toor"
-dbname = "P3_ANDROID_STUDIO"
+# Nombre de usuario del SGBD
+user ="avnadmin"
+# Contraseña del usuario del SGBD
+password = "AVNS_NIKxikhltFInnUgm-rG"
+# Nombre de la BD
+dbname = "defaultdb"
 # URL de la BD
-SQLALCHEMY_DATABASE_URL = f"mysql+mysqlconnector://{user}:{password}@localhost:3306/{dbname}"
+SQLALCHEMY_DATABASE_URL = f"mysql+mysqlconnector://{user}:{password}@mysql-1296e07c-aterik-c203.j.aivencloud.com:13888/{dbname}?charset=utf8mb4"
 # Motor de conexion de la bd
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+ssl_args = {
+    'ssl': {
+        'sslmode': 'REQUIRED',
+        'ca': '/app/database/ca.pem'
+    }
+}
+engine = create_engine(SQLALCHEMY_DATABASE_URL, ssl_args)
 # Crea el generador de sesiones de la BD
 sessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 # Modelos de la BD
